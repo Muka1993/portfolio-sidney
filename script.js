2707fa2322c90753bed8eed4a69dae208573349d
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2024-05-26 10:14:17 -0300

    Projeto em andamento

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      // especifique opções aqui
+    });
+  });
+          
